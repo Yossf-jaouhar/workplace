43f4a47e58b70f948d35206253e483fab7696037
Yossf-jaouhar <jwhryosf@gmail.com>
2024-11-12 20:29:20 +0100

    lc

---
+++ leet_code/diagonalDifference/diagonalDifference.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+
+func main() {
+	arr := [][]int32{
+		{11, 2, 4},
+		{4, 5, 6},
+		{10, 8, -12},
+	}
+
+	result := diagonalDifference(arr)
+	fmt.Println(result)
+}
+
+func diagonalDifference(arr [][]int32) int32 {
+		
+}
