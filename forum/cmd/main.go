833002c0cadd86ff942003c2904dfc5ba80313b4
Your Name <you@example.com>
2025-01-26 23:58:21 +0100

    writing the sql file

--- forum/cmd/main.go
+++ forum/cmd/main.go
@@ -2,13 +2,15 @@ package main
 
 import (
 	"fmt"
+	routes "forum/cmd/routers"
+	"forum/database"
 	"net/http"
 )
 
 func main() {
 
-	//InitDatabase(dbPath string)
-	RegisterRoutes()
+	database.InitDatabase()
+	routes.RegisterRoutes()
 	fmt.Println("your serve on : http://localhost:8080")
 	http.ListenAndServe(":8080", nil)
 }
